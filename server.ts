9699000ca206143c3d5dc2ce540e84097b2182bd
xania <ibrahim.bensalah@gmail.com>
2019-02-09 17:30:31 +0100

    puppeteer arbeidsrecht

--- server.ts
+++ server.ts
@@ -5,6 +5,91 @@ import * as express from "express"
 import { Application } from "express"
 import transform from "./transform.js"
 import httpProxy from "http-proxy"
+import puppeteer from 'puppeteer';
+
+const Arbeidsrecht = (async () => {
+  const browser = await puppeteer.launch({
+    args: ['--disable-features=site-per-process'],
+    headless: false
+  });
+  const page = await browser.newPage();
+  await page.goto('https://arbeidsrecht.sdu.nl');
+  // await page.screenshot({path: 'example.png'});
+  // await delay(100);
+  await page.waitForSelector(`#cmp-faktor-io`);
+  //  console.log(page.frames().map(f => f));
+  //  var faktorFrame = await page.$('#cmp-faktor-io');;
+  // faktorFrame.remove();
+  // console.log("wait for acceptAll")
+  //  const acceptAll = await faktorFrame.$('#acceptAll');
+  //   const outerHTML = await acceptAll.evaluate(e => e.parentNode.outerHTML);
+  //  console.log('the outerhtml: ', outerHTML);
+  // to verify we're talking about the right element.. (we are.)
+  //  await acceptAll.click();
+
+  // await faktorFrame.waitForSelector(`#acceptAll`);
+  // await delay(3000);
+  // const acceptAllBtn = await faktorFrame.$('#acceptAll');
+
+  return async function (term: string) {
+    await page.evaluate(() => {
+      const faktor = document.querySelector("iframe#cmp-faktor-io");
+      faktor && faktor.remove();
+    })
+
+    console.log("input");
+    await page.waitForSelector(`input[name='a$word0']`);
+    console.log("type");
+    const elementHandle = await page.$(`input[name='a$word0']`);
+    var currentValue = await page.evaluate(e => e.value, elementHandle);
+    if (currentValue !== term) {
+      await elementHandle.click();
+      await elementHandle.focus();
+      // click three times to select all
+      await elementHandle.click({ clickCount: 3 });
+      await elementHandle.press('Backspace');
+      await elementHandle.type(term);
+
+      // await page.type(`input[name='a$word0']`, term);
+      console.log("click");
+      await page.click(`form.simple-search-bar-form button[type='submit']`);
+      await page.waitForSelector(`.results-loading`);
+    }
+    await page.waitForSelector(`.results-loaded`);
+    // await delay(1000);
+
+    return await page.evaluate(() => {
+      const contents = document.querySelectorAll(".result-item .result-content");
+      const results = [];
+
+      for(var i=0 ; i<contents.length ; i++) {
+        const content = contents[i];
+        const anchor = content.querySelector("a.title") as HTMLAnchorElement;
+
+        const data = {
+          title: anchor.textContent,
+          url: anchor.href,
+          properties: []
+        }
+
+        const summary = content.querySelector(".urinfo-summary");
+        if (summary) {
+          data.properties.push(summary.textContent);
+        }
+        // for(var i=0 ; i<labels.length ; i++) {
+        // //   const childNode = labels[i];
+        // //   if (childNode.nextSibling) {
+        // //     data.properties.push(childNode.textContent);
+        // //   }
+        // }
+
+        results.push(data);
+      }
+
+      return results;
+    });
+  }
+})();
 
 const createApplication: () => Application = express['default'];
 var app: Application = createApplication();
@@ -30,6 +115,20 @@ if (fs.existsSync('proxy.json')) {
     })
   }
 }
+
+app.get('/api/arbeidsrecht/:term', async (req, res, next) => {
+  var search = await Arbeidsrecht;
+  try {
+    const results = await search(req.params.term);
+    res.setHeader("Content-Type", "text/javascript");
+    res.write(JSON.stringify(results));
+    res.end();
+  }
+  catch (ex) {
+    console.log(ex);
+    res.end();
+  }
+});
 
 app.get('/*.js', async (req, res, next) => {
   const js = path.resolve("." + req.path);
@@ -68,3 +167,11 @@ server.listen(8080, function () {
 //     var fullpath = resolve.sync(`./src/app`, { basedir })
 //     transform(fullpath, { write: noop, end: _ => _ });
 // }
+
+function delay<T>(sec: number) {
+  return new Promise<T>(resolve => {
+    setTimeout(_ => {
+      resolve();
+    }, sec);
+  });
+}
